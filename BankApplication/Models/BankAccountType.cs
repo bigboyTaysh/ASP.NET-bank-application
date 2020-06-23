795e36f22771bebc4a6723585be93da92541e067
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-23 20:49:10 +0200

    [add] - add models

---
+++ BankApplication/Models/BankAccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.Models
+{
+    public class BankAccountType
+    {
+        public int ID { get; set; }
+        public decimal Commission { get; set; }
+        public string TypeName { get; set; }
+    }
+}
---
+++ BankApplication/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.Models
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+        public string Login { get; set; }
+        public virtual List<BankAccount> BankAccounts { get; set; }
+    }
+}
