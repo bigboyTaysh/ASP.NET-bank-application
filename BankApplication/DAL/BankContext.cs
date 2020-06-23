387e7ec8fe69963fa55de2640c675a0bd8f4ea9e
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-23 20:54:10 +0200

    [add] - bank db context

---
+++ BankApplication/DAL/BankContext.cs
@@ -0,0 +1,25 @@
+﻿using BankApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.DAL
+{
+    public class BankContext : DbContext
+    {
+        public BankContext() : base("DefaultConntection")
+        {
+        }
+        public DbSet<BankAccountType> BankAccountTypes { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
