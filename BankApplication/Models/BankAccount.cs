36dfe42bbd4aaa8cab1f8d9dd03fd4f159637ad2
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-23 20:49:10 +0200

    [add] - add models

---
+++ BankApplication/Models/BankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.Models
+{
+    public class BankAccount
+    {
+        public int ID { get; set; }
+        public decimal Balance { get; set; }
+        public decimal AvailableFounds { get; set; }
+        public decimal Lock { get; set; }
+        public string BankAccountNumber { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int BankAccountTypeID { get; set; }
+
+        public virtual BankAccountType BankAccountType { get; set; }
+    }
+}
