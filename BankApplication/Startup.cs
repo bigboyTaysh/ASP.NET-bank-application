3c9464f04a8553db9a6057c634fb8e218ed3a108
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-23 20:44:30 +0200

    Dodaj pliki projektów.

---
+++ BankApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankApplication.Startup))]
+namespace BankApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
