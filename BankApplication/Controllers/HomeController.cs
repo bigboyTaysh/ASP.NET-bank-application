a4d517d9654b81e7b5113c7d91000f93337185e3
bigboyTaysh <pvtryk.w@gmail.com>
2020-06-25 22:32:54 +0200

    [change] - view statistics with groupby in About

--- BankApplication/Controllers/HomeController.cs
+++ BankApplication/Controllers/HomeController.cs
@@ -3,22 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BankApplication.DAL;
 using BankApplication.Models;
+using BankApplication.ViewModels;
 
 namespace BankApplication.Controllers
 {
     public class HomeController : Controller
     {
+        private BankContext db = new BankContext();
         public ActionResult Index()
         {
-            return View(new NotifiactionCount { Name = "Bacon" });
+            return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<CreationDateGroup> data = from bankAccount in db.BankAccounts
+                                                 group bankAccount by bankAccount.CreationDate
+                                                 into dateGroup
+                                                 select new CreationDateGroup()
+                                                 {
+                                                     CreationDate = dateGroup.Key,
+                                                     BankAccountsCount = dateGroup.Count()
+                                                 };
 
-            return View();
+            return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -26,6 +36,12 @@ namespace BankApplication.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
---
+++ BankApplication/ViewModels/CreationDateGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.ViewModels
+{
+    public class CreationDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? CreationDate { get; set; }
+        public int BankAccountsCount { get; set; }
+    }
+}
