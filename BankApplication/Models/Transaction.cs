18a5205fe32eae3fe8c53ee5ef8b7b608ebe79eb
bigboyTaysh <wolakv2@gmail.com>
2020-08-22 14:06:28 +0200

    [add] - transaction controller

--- BankApplication/Models/Transaction.cs
+++ BankApplication/Models/Transaction.cs
@@ -14,19 +14,16 @@ namespace BankApplication.Models
         [Column(TypeName = "money")]
         public decimal Value { get; set; }
 
-        [Required]
         [Display(Name = "Saldo")]
         [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
         [Column(TypeName = "money")]
         public decimal BalanceAfterTransactionUserFrom { get; set; }
 
-        [Required]
         [Display(Name = "Saldo")]
         [DisplayFormat(DataFormatString = "{0:0.00}", ApplyFormatInEditMode = true)]
         [Column(TypeName = "money")]
         public decimal BalanceAfterTransactionUserTo{ get; set; }
 
-        [Required]
         [Display(Name = "Nadawca")]
         public string FromBankAccountNumber { get; set; }
 
@@ -37,7 +34,7 @@ namespace BankApplication.Models
         [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        [Display(Name = "Creation Date")]
+        [Display(Name = "Data przelewu")]
         public DateTime Date { get; set; }
 
         [Required]
