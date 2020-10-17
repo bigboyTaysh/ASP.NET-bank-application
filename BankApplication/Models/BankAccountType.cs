755a9212c9c9f5c62327acc17fd69ae4f8b532d5
bigboyTaysh <wolakv2@gmail.com>
2020-10-17 23:03:43 +0200

    add register controller override

---
+++ BankApplication/Models/BankAccountType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankApplication.Models
+{
+    public class BankAccountType
+    {
+        public int ID { get; set; }
+        public decimal Commission { get; set; }
+        public string Type { get; set; }
+    }
+}
