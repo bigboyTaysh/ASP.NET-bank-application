667d4ce02faca828b40bdd29769272551cc42bd4
Patryk Wolski <pvtryk.w@gmail.com>
2020-12-21 10:39:00 +0100

    configure initializer

---
+++ Shop/Data/ShopInicialzier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Shop.Models;
+
+namespace Shop.Data
+{
+    public class ShopInicialzier
+    {
+        public static async Task SeedAsync()
+        {
+
+        }
+    }
+}
