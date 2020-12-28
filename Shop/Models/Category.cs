88d851446da3bca92d780c22c26123293a1143f5
Patryk Wolski <pvtryk.w@gmail.com>
2020-12-28 16:26:08 +0100

    product controller, return product categories by id

--- Shop/Models/Category.cs
+++ Shop/Models/Category.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Shop.Models
@@ -9,6 +10,7 @@ namespace Shop.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        [JsonIgnore]
         public List<ProductCategory> ProductCategories { get; set; }
     }
 }
--- Shop/Models/ProductCategory.cs
+++ Shop/Models/ProductCategory.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Shop.Models
 {
     public class ProductCategory
     {
+        [JsonIgnore]
         public int ProductID { get; set; }
+        [JsonIgnore]
         public virtual Product Product { get; set; }
+        [JsonIgnore]
         public int CategoryID { get; set; }
         public virtual Category Category { get; set; }
     }
