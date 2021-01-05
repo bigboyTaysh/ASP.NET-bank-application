152a67eb9f29c3b3936bc209237c54c76f0f8362
bigboyTaysh <pvtryk.w@gmail.com>
2021-01-05 18:18:08 +0100

    payment card model

---
+++ BankApplication/Models/PaymentCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BankApplication.Models
+{
+    public class PaymentCard
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Numer karty")]
+        public string PaymentCardNumber { get; set; }
+        [Range(1000, 9999)]
+        public int Code { get; set; }
+        public bool Blocked { get; set; }
+        public bool SecureCard { get; set; }
+    }
+}
